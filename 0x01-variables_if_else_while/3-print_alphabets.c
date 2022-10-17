6f5c8b2940ebae0ed704909c0120875ca509e812
Dread=IQ <hamzayare69@gmail.com>
2022-10-17 14:50:17 -0700

    W

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,20 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - printing alphabets in lowercase                                                                                               
+#include <stdio.h>
+/**
+ * main - printing alphabets in lowercase and uppercase
  *
  * Return:0
- */   
+ */
 
 int main(void)
 {
 	int ch;
-	clrscr();
 
-	for (ch= ‘a’; ch<= ‘z’; ch++)
+	for (ch = ‘a’; ch <= ‘z’; ch++)
 		putchar(ch);
 
-	for (ch= ‘A’; ch<= ‘Z’; ch++)
+	for (ch = ‘A’; ch <= ‘Z’; ch++)
 		putchar(ch);
 
 	putchar("\n");
-	return(0);
+	return (0);
 }
