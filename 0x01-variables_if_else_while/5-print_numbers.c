f988b502ce2629ecf29525b60258603294b52097
Dread=IQ <hamzayare69@gmail.com>
2022-10-17 15:29:38 -0700

    W

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printong single digit base 10 nums
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; no++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
