a920f6759a1c3278d3c3f99b12c859b6ab9e36b1
Dread=IQ <hamzayare69@gmail.com>
2022-10-17 14:09:18 -0700

    W

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h
+
+/**
+ * main - printing alphabets in lowercase
+ *
+ * Return:0
+ */
+int main(void)
+{
+	    char alphabet = 'a';
+
+	    while (alphabet <= 'z')
+	    {
+		    putchar(alphabet);
+		    alphabet++;
+	    }
+		putchar('\n');
+			return (0);
+}
