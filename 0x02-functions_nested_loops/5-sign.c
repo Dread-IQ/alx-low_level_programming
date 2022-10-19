d2b998f393497506f9620090608b580c9508ab33
Dread=IQ <hamzayare69@gmail.com>
2022-10-19 16:28:30 -0700

    W

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a NO.
+ * @n: The NO of which the sign will be printed.
+ *
+ * Return: 1 if the No is greater than zero, Return: 1 if the NO is greater than zero, -1 if the NO is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0) {
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0) {
+		_putchar('0');
+		return (0);
+	}
+	else {
+		_putchar('-');
+		return (0);
+	}
+}
