8f5bac120ed94f77555e621db21825cccee7bd86
Dread=IQ <hamzayare69@gmail.com>
2022-10-19 16:36:15 -0700

    W

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,22 +4,23 @@
  * @n: The NO of which the sign will be printed.
  *
  * Return: 1 if the No is greater than zero,
- * Return: 1 if the NO is greater than zero,
+ * 0 if the NO is greater than zero,
  * And return -1 if the NO is less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
-       	{
+	{
 		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
-       	{
+	{
 		_putchar('0');
 		return (0);
 	}
-	else {
+	else
+	{
 		_putchar('-');
 		return (0);
 	}
