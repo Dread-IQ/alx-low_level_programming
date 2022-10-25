ca1a7ec021d7f876e3a50a1c81981c5a5e632020
Dread=IQ <hamzayare69@gmail.com>
2022-10-25 14:11:07 -0700

    GM

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "hamza.h"
+
+/**
+ * print_rev - Prints a reversed string.
+ * @s: The string.
+ */
+
+void print_rev(char *s)
+
+{
+	int rs = 0, index;
+	
+	while (s[index++])
+		rs++;
+	
+	for (index = rs - 1; index >= 0; index--)
+		_putchar(s[index]);
+	
+	_putchar('\n');
+}
