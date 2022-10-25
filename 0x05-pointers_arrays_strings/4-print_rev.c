7c2e4620d3275c5dd1bddcb35730b69b9db4e540
Dread=IQ <hamzayare69@gmail.com>
2022-10-25 14:13:58 -0700

    GM

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,12 @@ void print_rev(char *s)
 
 {
 	int rs = 0, index;
-	
+
 	while (s[index++])
 		rs++;
-	
+
 	for (index = rs - 1; index >= 0; index--)
 		_putchar(s[index]);
-	
+
 	_putchar('\n');
 }
