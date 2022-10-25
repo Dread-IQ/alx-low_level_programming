1eeaefe5e6f4b73d2c76ca2b72b63597beeffdcb
Dread=IQ <hamzayare69@gmail.com>
2022-10-25 14:04:54 -0700

    GM

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "hamza.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string.
+ */
+
+void _puts(char *str)
+
+{
+	while (*str)
+		_putchar(*str++);
+	
+	_putchar('\n');
+}
