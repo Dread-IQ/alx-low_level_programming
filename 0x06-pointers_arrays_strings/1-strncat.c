025bd9c86389ed34c7b5ed7121778e199af3f916
Dread=IQ <hamzayare69@gmail.com>
2022-10-26 04:16:16 -0700

    EW

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "hamza.h"
+
+/**
+ * _strncat - Concatenates two strings using at most
+ * 	       an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_rs = 0;
+
+	while (dest[index++])
+		dest_rs++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_rs++] = src[index];
+
+	return (dest);
+}
